a2cd39e59b6e9909da6771b8688e819bbf0afff6
Rangga Rifqi <rangga@ranggarifqi.com>
2023-02-27 22:34:23 +0700

    Setup crawler

--- cmd/crawlers/ikhwan_1/main.go
+++ cmd/crawlers/ikhwan_1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/gocolly/colly"
 	"github.com/ranggarifqi/islamic-name-generator-be/mongodb"
 	"github.com/ranggarifqi/islamic-name-generator-be/src/name"
 )
@@ -21,17 +22,51 @@ func main() {
 	nameRepository := name.NewMongoRepository(ctx, mongoDB)
 	nameService := name.NewService(nameRepository)
 
-	// TODO: Crawl the page & get the names, meanings, etc
-	result, err := nameService.UpsertName(name.Name{
-		Name:      "Test 1",
-		NameTypes: []name.NameType{name.FIRST_NAME},
-		Gender:    name.IKHWAN,
-		Meanings:  []string{"G ada", "testzzz", "wololo"},
+	fmt.Println(nameService)
+
+	// https://www.detik.com/sulsel/berita/d-6529117/1350-nama-bayi-laki-laki-islami-lengkap-beserta-artinya
+	c := colly.NewCollector(
+		colly.AllowedDomains(
+			"detik.com",
+			"https://detik.com",
+			"www.detik.com",
+		),
+	)
+
+	c.OnHTML("li", func(h *colly.HTMLElement) {
+		class := h.Attr("class")
+
+		if class != "" {
+			return
+		}
+
+		fmt.Printf("name & meaning found: %v\n", h.Text)
+
+		// Split the name & the meaning by character ":"
+		// Treat them all as FIRST_NAME, MIDDLE_NAME, and LAST_NAME
+		// lower case them all (name & meaning)
+		// Set gender = IKHWAN
 	})
 
-	if err != nil {
-		panic(err)
-	}
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
 
-	fmt.Printf("result = %v\n", result)
+	c.Visit("https://www.detik.com/sulsel/berita/d-6529117/1350-nama-bayi-laki-laki-islami-lengkap-beserta-artinya/")
+
+	c.Wait()
+
+	// TODO: Crawl the page & get the names, meanings, etc
+	// result, err := nameService.UpsertName(name.Name{
+	// 	Name:      "Test 1",
+	// 	NameTypes: []name.NameType{name.FIRST_NAME},
+	// 	Gender:    name.IKHWAN,
+	// 	Meanings:  []string{"G ada", "testzzz", "wololo"},
+	// })
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("result = %v\n", result)
 }
